be80be0a4000d880b3a97292235daee4e50b2c43
Nina Woesten <105288781+NinaWoesten@users.noreply.github.com>
2024-04-30 10:21:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,110 +1,119 @@
-import asyncio
-import openai
+import os
+import json
+
+import time
+from dotenv import load_dotenv
 from settings import API_KEY, API_ID, API_HASH, BOT_TOKEN, bot_name, model_engine
-from telethon import TelegramClient, events
-
-# Konfiguration API Key
-openai.api_key = API_KEY
-print(API_KEY)
-
-
-client = TelegramClient('telegaBot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
-
-
-async def send_input_and_get_response(prompt, conversation):
-    message = await conversation.send_message(prompt)
-    task_wait = asyncio.create_task(conversation.wait_event(events.CallbackQuery()))
-    task_respond = asyncio.create_task(conversation.get_response())
-
-    done, _ = await asyncio.wait(
-        {
-            task_wait, task_respond
-        },
-        return_when=asyncio.FIRST_COMPLETED)
-    
-    response = done.pop().result()
-    #Nachricht wird gelöscht sobald eine Antwort erhalten wurde
-    await message.delete()
-    #None wenn Antwort ein Callback ist
-    if isinstance(response, events.CallbackQuery.Event):
-        return None
-    else:
-        return response.message.strip()
-        
-
-@client.on(events.NewMessage(pattern="(?i)/start"))
-async def respond(event):
-    try:
-        async with client.conversation(await event.get_chat(), timeout=600) as conversation:
-            history = []
-
-            while True:
-                user_input = event.message.text
-                response = await send_input_and_get_response(user_input, conversation)
-                
-                #User beendet Konversation
-                if user_input is None:
-                    prompt ="Conversation ended. Type /start to start a new one"
-                    await client.send_message(prompt)
-                    break
-                else:
-                    #prompt = "I'm thinking about the response..."
-                    #thinking_message = await client.send_message(prompt)
-
-                    history.append({"role": "user", "content": user_input})
-
-                    chat_completion = openai.ChatCompletion.create(
-                        model= model_engine,
-                        messages = history,
-                        max_tokens=500,
-                        n=1,
-                        temperature=0.1
-                    )
-                    response = chat_completion.choices[0].message.content
-                    history.append({"role": "bot", "content": response})
-                    await thinking_message.delete()
-                    await client.send_message(response, parse_mode='Markdown')
-
-    #Zeitüberschreitungsfehler
-    except asyncio.TimeoutError:
-        await event.send_message("Timeout: Conversation ended.")
-    #Andere Ausnahmen
-    except Exception as e:
-        print(f"Error: {e}")
-        await event.respond("Sorry, something went wrong.")
-
-
-if __name__ == "__main__":
-        #Start client
-        print("Starting...")
-        client.run_until_disconnected()
-
-
-'''
-# Erstellen eines OpenAI-Clients
-client = openai.completions.create(
-    engine="davinci-codex",
-    prompt="Hey",
-    temperature=1,
-    max_tokens=2048,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
+from telegram import (
+    ReplyKeyboardMarkup,
+    Update,
+    KeyboardButton, 
 )
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+from openai import OpenAI
 
-# Ausführen der Anfrage
-response = client
+client = OpenAI(api_key=API_KEY)
+print(API_KEY)
+(ENTRY_STATE, 
+QUESTION_STATE,
+) = range(2)
 
-print(response)
-client = TelegramClient(settings.name_bot, settings.API_ID, settings.API_HASH).start(bot_token=settings.BOT_TOKEN)
-#keyboard_stop = [[buttons.inline ("Stop and delete conversation for conversation", b "stop" )]]
+class Bot:
+    def get_response(self, question):
+        prompt = question
+        load_dotenv()
+        client.api_key = API_KEY
+        response = client.completions.create(
+            model=model_engine,
+            prompt=prompt,
+            max_tokens=51,
+            temperature=0.5
+        )
+        json_object = response
 
-@client.on(events.NewMessage(pattern="(?i)/start"))
-async def handle_start_command(event):
-    SENDER = event.sender_id
+        json_string = json.dumps(json_object)
+        parsed_json = json.loads(json_string)
+        text = parsed_json['choices'][0]['text']
+        cleared_text = self.clear_text(text)
+        return cleared_text
+
+
+def _generate_bot(prompt:str):
+    bot = Bot()
+    b = bot.get_response(prompt)
+    return b
+
+async def start(update: Update, context: ContextTypes):
+    button = [[KeyboardButton(text="Ask-a-question")]]
+    reply_markup = ReplyKeyboardMarkup(
+        button, resize_keyboard=True
+    )
+    await update.message.reply_text(
+        "Hello, would you like to ask me a question? "
+        "Choose an option to continue",
+        reply_markup=reply_markup,
+    )
+    return ENTRY_STATE
+
+async def pre_query_handler(update: Update, context: ContextTypes):
+    button = [[KeyboardButton(text="Back")]]
+    reply_markup = ReplyKeyboardMarkup(
+        button, resize_keyboard=True
+    )
+    await update.message.reply_text(
+        "Enter text",
+        reply_markup=reply_markup,
+    )
+    return QUESTION_STATE
+
+async def pre_query_response_handler(update: Update, context: ContextTypes):
+    try:
+        button = [[KeyboardButton(text="Back")]]
+        reply_markup = ReplyKeyboardMarkup(
+            button, resize_keyboard=True
+        )
+        question = update.message.text 
+        response = _generate_bot(question)
+        context.user_data['response'] = response
+
+        await update.message.reply_text(
+            response, reply_markup=reply_markup,
+        )
+        return QUESTION_STATE
+    except Exception as e:
+        # Fehlermeldung an den Benutzer senden
+        error_message = "Sorry, something went wrong."
+        await update.message.reply_text(error_message)
+        # Fehler protokollieren
+        print(f"Error: {e}")
+
 
 if __name__ == "__main__":
-    print("Starting...")
-    client.run_until_disconnected()
-: '''
+    load_dotenv()
+    application = Application.builder().token(BOT_TOKEN).read_timeout(100).get_updates_read_timeout(100).build()
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            ENTRY_STATE:[
+                MessageHandler(filters.Regex('^Back$'), start),
+                MessageHandler(filters.Regex('^Ask-a-question$'), pre_query_handler),
+            ],
+            QUESTION_STATE: [
+                MessageHandler(filters.Regex('^Back$'), start),
+                MessageHandler(filters.TEXT, pre_query_response_handler),
+            ],
+        },
+        fallbacks=[],
+    )
+    application.add_handler(conv_handler)
+    print("Starting....")
+    application.run_polling()
+
 
